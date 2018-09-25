c2e72723c1e11c3a53dd5dcc70d28fe10917658d
GetUp100 <lorenz.gross@informatik.hs-fulda.de>
2018-09-25 14:52:17 +0200

    Verantwortungsbereich Lorenz Groß
    Dies ist ein nachträglicher Upload der Scripte aus meinem Verantwortungsbereichs des Projektes.
    Der Upload findet per Browser statt weil der Upload per Github seit 3 Wochen nicht möglich war (100MB Datei Upload Limit).

---
+++ rotationColor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationColor : MonoBehaviour
+{
+
+    float smooth = 0.6f;
+
+
+
+    void Update()
+    {
+        // Smoothly tilts a transform towards a target rotation.
+        
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            transform.Rotate(0, 180, 0);
+            smooth = 3f;
+        }
+
+        Quaternion target = Quaternion.Euler(180, 0, 0);
+
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+    }
+}
